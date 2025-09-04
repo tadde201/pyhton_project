3a0b8cbffc1a3f96411ae53c66e48e306c436a5e
tadd2018 <tadde2018@gmail.com>
2025-09-04 13:42:24 -0400

    Postgress

---
+++ PosgresSQl.sql
@@ -0,0 +1,18 @@
+SELECT * from "TB1"
+  INSERT INTO TB1 (id, Name, Location)
+VALUES
+  (1, 'Lemma', 'ethiopia'),
+  (2, 'mike', 'jordan'),
+  (3, 'john', 'doe');
+  Create table test(
+    id int,
+    Name varchar(20),
+    Location varchar(20)
+  );
+  select * from test
+    INSERT INTO test (id, Name, Location)
+VALUES
+  (1, 'Lemma', 'ethiopia'),
+  (2, 'mike', 'jordan'),
+  (3, 'john', 'doe');
+  select * from test
