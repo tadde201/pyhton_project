dd813f92dc2b298147e019b9e04df5ac7aa9dcea
tadd2018 <tadde2018@gmail.com>
2025-09-05 09:58:06 -0400

    data change

--- AllSQLcode.sql/AIRline.sql
+++ AllSQLcode.sql/AIRline.sql
@@ -7,3 +7,19 @@ select system ,
      count(selected) as numberseclete 
 from ari_enterprice.system_selected_tableau
 group by system ,selected
+
+
+select  model,x_axis,y_axis,
+count(
+	case when x_axis='Critical' then 1
+	else 0
+	end )as x_axisnumber
+	,
+	count
+	(case when y_axis='Critical' then 1
+	else 0
+	end )as y_axisnumber
+ from ari_enterprice.weight_all_models
+  Group by model,x_axis,y_axis
+  
+  
