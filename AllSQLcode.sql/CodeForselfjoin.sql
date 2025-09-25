17c5b2455164caa8972f4166445c87f2b6583633
tadd2018 <tadde2018@gmail.com>
2025-09-25 07:30:10 -0400

    Data change made

--- AllSQLcode.sql/CodeForselfjoin.sql
+++ AllSQLcode.sql/CodeForselfjoin.sql
@@ -14,8 +14,9 @@ INSERT INTO ipl_team (team_name) VALUES
 
 select * 
 from ipl_team  as t1
-Join ipl_team  as t2
+    Join ipl_team  as t2
 on t1.team_name<t2.team_name
+
 Create table Selfjoindata
 ( team_name1 varchar(10),
  team_name2 varchar(10)
@@ -24,12 +25,16 @@ INSERT INTO Selfjoindata (team_name1,team_name2)
 select * 
 from ipl_team  as t1
     Join ipl_team  as t2
-on t1.team_name<t2.team_name;
-
+on t1.team_name<t2.team_name
 select * from Selfjoindata;
+
 Select team_name1,count(team_name1) as numOccurence 
 from Selfjoindata
 Group by team_name1
 order by count(team_name1) Desc
+
+
+select * from selfjoindata
+where team_name1='MI';
 
 
