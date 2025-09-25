b9c5d210a334c61017f5c0382e5cbfa36169b156
tadd2018 <tadde2018@gmail.com>
2025-09-25 06:58:34 -0400

    Data has been changed

--- AllSQLcode.sql/CodeForselfjoin.sql
+++ AllSQLcode.sql/CodeForselfjoin.sql
@@ -16,6 +16,20 @@ select *
 from ipl_team  as t1
 Join ipl_team  as t2
 on t1.team_name<t2.team_name
+Create table Selfjoindata
+( team_name1 varchar(10),
+ team_name2 varchar(10)
+);
+INSERT INTO Selfjoindata (team_name1,team_name2)
+select * 
+from ipl_team  as t1
+    Join ipl_team  as t2
+on t1.team_name<t2.team_name;
 
+select * from Selfjoindata;
+Select team_name1,count(team_name1) as numOccurence 
+from Selfjoindata
+Group by team_name1
+order by count(team_name1) Desc
 
 
