2d8c9053f10517a617f4a1062c75559be50f05cb
tadd2018 <tadde2018@gmail.com>
2026-05-02 11:56:38 -0400

    SQL Code  formated

--- Substringagg for change column to row.sql
+++ Substringagg for change column to row.sql
@@ -1,16 +1,48 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [Entity]
-      ,[Code]
-      ,[Daily hospital occupancy]
-      ,[Days]
-  FROM [Covid].[dbo].[covidt]
-  create table covid2(Entity Varchar(60),code varchar(60),Daily_hospital_occupancy float,  days date)
-  insert into covid2
-  select * from [Covid].[dbo].[covidt]
-  select * from covid2
-  update covid2
-  set code =''
-  select * from [dbo].[emp]
-  select 
-  STRING_AGG(name,',') ,STRING_AGG(Salary,',') 
-  from [dbo].[emp]
+/****** Script for SelectTopNRows command from SSMS ******/
+
+-- Select top 1000 rows
+SELECT TOP (1000)
+    [Entity],
+    [Code],
+    [Daily hospital occupancy],
+    [Days]
+FROM [Covid].[dbo].[covidt];
+
+
+-- Create new table
+CREATE TABLE covid2 (
+    Entity VARCHAR(60),
+    Code VARCHAR(60),
+    Daily_hospital_occupancy FLOAT,
+    Days DATE
+);
+
+
+-- Insert data into new table
+INSERT INTO covid2 (Entity, Code, Daily_hospital_occupancy, Days)
+SELECT
+    [Entity],
+    [Code],
+    [Daily hospital occupancy],
+    [Days]
+FROM [Covid].[dbo].[covidt];
+
+
+-- View data
+SELECT * FROM covid2;
+
+
+-- Update all codes to empty string
+UPDATE covid2
+SET Code = '';
+
+
+-- View employee table
+SELECT * FROM [dbo].[emp];
+
+
+-- Aggregate names and salaries
+SELECT 
+    STRING_AGG(Name, ',') AS Names,
+    STRING_AGG(CAST(Salary AS VARCHAR), ',') AS Salaries
+FROM [dbo].[emp];
--- Supstring cleaning.sql
+++ Supstring cleaning.sql
@@ -1,28 +1,57 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [empserial]
-      ,[name]
-      ,[salary]
-  FROM [project1].[dbo].[emp2]
-  alter table [project1].[dbo].[emp2]
-  add address char(40)
-  select * from emp2
-  update emp2
-  set address='16735,U street,DC'
-  where empserial=6
-  select substring(address,1,CHARINDEX(',',address)-1) as addresse,
-  substring(address,CHARINDEX(',',address)+1,len(address)) as namesaddress
+/****** Script for SelectTopNRows command from SSMS ******/
 
-  
-  from emp2
+-- Select top 1000 rows
+SELECT TOP (1000)
+    [empserial],
+    [name],
+    [salary]
+FROM [project1].[dbo].[emp2];
 
-  alter table emp2
-  add address1 varchar(60)
-  alter table emp2
-  add namesaddress varchar(60)
-  update emp2
-  set address1=substring(address,1,CHARINDEX(',',address)-1) 
-   update emp2
-  set namesaddress=substring(address,CHARINDEX(',',address)+1,len(address)) 
-    select * from emp2
-	alter table emp2
-	drop column address
+
+-- Add address column
+ALTER TABLE [project1].[dbo].[emp2]
+ADD address CHAR(40);
+
+
+-- View table
+SELECT * 
+FROM [project1].[dbo].[emp2];
+
+
+-- Update specific record
+UPDATE [project1].[dbo].[emp2]
+SET address = '16735,U street,DC'
+WHERE empserial = 6;
+
+
+-- Split address into two parts
+SELECT 
+    SUBSTRING(address, 1, CHARINDEX(',', address) - 1) AS address1,
+    SUBSTRING(address, CHARINDEX(',', address) + 1, LEN(address)) AS namesaddress
+FROM [project1].[dbo].[emp2];
+
+
+-- Add new columns for split data
+ALTER TABLE [project1].[dbo].[emp2]
+ADD address1 VARCHAR(60);
+
+ALTER TABLE [project1].[dbo].[emp2]
+ADD namesaddress VARCHAR(60);
+
+
+-- Update new columns with split values
+UPDATE [project1].[dbo].[emp2]
+SET address1 = SUBSTRING(address, 1, CHARINDEX(',', address) - 1);
+
+UPDATE [project1].[dbo].[emp2]
+SET namesaddress = SUBSTRING(address, CHARINDEX(',', address) + 1, LEN(address));
+
+
+-- View final table
+SELECT * 
+FROM [project1].[dbo].[emp2];
+
+
+-- Drop original address column
+ALTER TABLE [project1].[dbo].[emp2]
+DROP COLUMN address;
